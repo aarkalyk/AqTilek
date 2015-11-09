3b9a7f94dfabfaa3b5de8e83d44c780e5bde79bd
Marat <Marat@macbook-pro-marat.local>
2015-11-10 00:30:07 +0600

    Initial Commit

---
+++ AkTilek/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  AkTilek
+//
+//  Created by Marat on 10.11.15.
+//  Copyright © 2015 GrownApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ AkTilek/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AkTilek
+//
+//  Created by Marat on 10.11.15.
+//  Copyright © 2015 GrownApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
